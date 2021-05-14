dd57b6c46ce17741e52101ea8d36e551fb1f6504
Doko85 <marcomazzeo985@gmail.com>
2021-05-14 14:17:28 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCmGJEpSfjKNBrVYUjPKB6kKh-oJwhpsMs",
+    authDomain: "firecollectionsapp.firebaseapp.com",
+    projectId: "firecollectionsapp",
+    storageBucket: "firecollectionsapp.appspot.com",
+    messagingSenderId: "387968407940",
+    appId: "1:387968407940:web:b633845f730db389a325d1",
+    measurementId: "G-DNM1YM89YM"
+  }
 };
